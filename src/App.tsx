a41be3a970d9b46b06f8f5d7435fff5eb7c8de16
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-11-03 22:48:21 +0600

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Task } from './interfaces/Task';
 import './App.css';
+import { TodoItem } from './components/TodoItem';
 
-function App() {
+const App: React.FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<Task[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.name === "task") {
+      setTask(e.target.value);
+    } else {
+      setDeadline(Number(e.target.value));
+    }
+  }
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
+
+    // Reset the input forms
+    setTask("");
+    setDeadline(0);
+  }
+
+  const deleteTask = (task: string): void => {
+    setTodoList(todoList.filter(t => t.taskName !== task));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="header"> 
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder="Task..."
+                name="task"
+                value={task}
+                onChange={handleChange}
+              />
+
+              <input
+                type="number"
+                placeholder="Deadline (in Days)..."
+                name="deadline"
+                value={deadline}
+                onChange={handleChange}
+              />
+            </div>
+            
+            <button onClick={addTask}>Add Task</button>
+        </div>
+        <div className="todo-list"> 
+            {todoList.map((t: Task, key: number) => {
+              return <TodoItem key={key} task={t} deleteTask={deleteTask}/>;
+            })}
+        </div>
     </div>
   );
 }
